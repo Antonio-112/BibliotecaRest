acf98277bef1bf662aff60fb58a5c43468aac6e6
Antonio <antonio.alexander.a10@gmail.com>
2020-11-26 14:06:19 -0300

    Initial commit
    Base de proyecto

---
+++ src/main/java/com/ant/BibliotecaRest/BibliotecaRestApplication.java
@@ -0,0 +1,13 @@
+package com.ant.BibliotecaRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaRestApplication.class, args);
+	}
+
+}
