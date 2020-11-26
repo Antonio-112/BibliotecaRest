ebd53046707bad68e5fac1d8c7a031c7151f510c
Antonio <antonio.alexander.a10@gmail.com>
2020-11-26 19:39:06 -0300

    Cambio en anotaciones

--- src/main/java/com/ant/BibliotecaRest/repository/BibliotecaRepository.java
+++ src/main/java/com/ant/BibliotecaRest/repository/BibliotecaRepository.java
@@ -2,14 +2,18 @@ package com.ant.BibliotecaRest.repository;
 
 import java.util.List;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.ant.BibliotecaRest.model.Libro;
 
+@Repository
+@ComponentScan
 public interface BibliotecaRepository extends CrudRepository<Libro, Integer> {
 
-	@Query("FROM Productos WHERE nombre = ?1")
+	@Query("FROM Libro WHERE nombre = ?1")
 	public List<Libro> findByNombre(String nombre);
 	
 }
